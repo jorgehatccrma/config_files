a60d774c17e576c336c2e5c4166d4f2a6e74eb97
Jorge Herrera <jorge.herrera@shazam.com>
2018-03-30 16:25:46 -0700

    fixing color stuff

--- colors.sh
+++ colors.sh
@@ -1,3 +1,5 @@
+# Display color codes (with the corresponding color)
+# It tries to organize the color in a visually consistent way
 cumwidth=7
 for i in {0..255}; do
   printf "\x1b[38;5;${i}m [%03i] \x1b[0m" $i ;
@@ -10,3 +12,19 @@ for i in {0..255}; do
     fi ;
   fi ;
 done
+
+echo ""
+
+# Another cool one
+awk 'BEGIN{
+for (colnum = 0; colnum<256; colnum++) {
+  r = 255-(colnum*255/255);
+  g = (colnum*510/255);
+  b = (colnum*255/255);
+  if (g>255) g = 510-g;
+  printf "\033[48;2;%d;%d;%dm", r,g,b;
+  printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+  printf "o\033[0m";
+}
+printf "\n";
+}'
